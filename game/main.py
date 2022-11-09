0b6df532904fab98c4001e15dc77bdd5c96f7055
arielperezti <ariel.perez.ti@gmail.com>
2022-11-08 22:47:30 -0500

    added game folder wich contains a sppc game

---
+++ game/main.py
@@ -0,0 +1,57 @@
+import random
+import re
+import math
+
+
+def choose_options():
+    options = ('SP', 'PT', 'CDF', 'PDLM', 'PQ', 'ME', 'M', 'E', 'S', 'L', 'M', 'MC', 'BA', 'FPT',
+               'LDM', 'OT', 'BI', 'P', 'J', 'SB', 'ET', 'L', 'RDA', 'JG', 'PCC', 'CH', 'OC', 'RDS')
+
+    user_option = input(""" Bienvenido al juego de la direccion
+    1. Escoge un miembro al azar
+    2. Salir """)
+
+    if not user_option:
+        print("Vacio")
+        return None
+
+
+    if user_option == 1:
+        total = len(options) + 1
+        computer_option = random.choice(range(total))
+
+        computer_option = options[computer_option]
+
+        print(computer_option)
+
+        if computer_option in ('FPT', 'SB', 'E'):
+            print('CRACK')
+
+        elif not computer_option in ('MC', 'BA', 'JG'):
+            print("Te salvaste! puro og")
+
+        else:
+            print('MALO! el mongosaurio chulito para el joven')
+    elif user_option == 2:
+        print('Bye')
+        return None
+
+    else:
+        print('Cualquiera')
+        return None
+
+
+def main():
+    while True:
+        choose_options()
+        op = input("""Quieres continuar:
+        1. Si
+        2. No""")
+
+        if op ==2:
+            return None
+        if op ==1:
+            pass
+
+if __name__ == "__main__":
+    main()
